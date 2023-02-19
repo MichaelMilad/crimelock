2419354f8db277df3e98469f0cccf3c41e8ed203
Michael Milad <michaelmilad001@gmail.com>
2023-02-19 13:39:39 +0200

    duplicated request for uk region

--- File Upload Script/index.js
+++ File Upload Script/index.js
@@ -3,18 +3,18 @@ const fs = require('fs');
 const formData = require('form-data');
 
 const contactsPost = {
-  "uploadRef": "a86fe496-e317-4842-862f-2e87f1773002",
+  "uploadRef": "1ad1b379-ab58-4d1f-8f0f-f266e47f568b",
   "uploadForm": {
       "url": "https://s3.ca-central-1.amazonaws.com/client-bestinsurance-trufla-sta",
       "fields": {
-          "Key": "trudocs/transient/external/a86fe496-e317-4842-862f-2e87f1773002",
+          "Key": "trudocs/transient/external/1ad1b379-ab58-4d1f-8f0f-f266e47f568b",
           "bucket": "client-bestinsurance-trufla-sta",
           "X-Amz-Algorithm": "AWS4-HMAC-SHA256",
-          "X-Amz-Credential": "ASIA246AQD4TVAQ25PHZ/20230207/ca-central-1/s3/aws4_request",
-          "X-Amz-Date": "20230207T211156Z",
-          "X-Amz-Security-Token": "FwoGZXIvYXdzEO///////////wEaDJClF9fFx4IQSLrKqiLkAdf/L1ZCu45Rl5p7kAFVu0Y/N2GlG+QO1w/TFIHJaNmW4dQIv0T/H0LDcSae32hWB9mWO47OkAwRU3tkODedP92mhYyUdrkWwqOPji99heDPOzmlS9dwe5ePlpKb5x9jStVefH7kyH2EnRdgmA0AZt+CgwG2xsDNmBkd6bcMTk8oftfU9eiJ4YfimkYLeuMPftSMy/KtD06cSJ8RNMAuBAiH8rN+hHlrASjfXOJeoILv37zYjNceeEwwHYe+fb3ws1GlqNmgohgup1f8nEVtz3pcTACnsK6inZ5qjq5nN115bh921yic/YqfBjItQyiUGXeswJWuRRwHZRoq1Z6466RaMex8yRkg71AeXkeiyyrp6BuASD8Mqz3n",
-          "Policy": "eyJleHBpcmF0aW9uIjoiMjAyMy0wMi0wN1QyMjoxMTo1NloiLCJjb25kaXRpb25zIjpbWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsMSwxMDQ4NTc2MDBdLFsic3RhcnRzLXdpdGgiLCIkQ29udGVudC1UeXBlIiwiIl0seyJLZXkiOiJ0cnVkb2NzL3RyYW5zaWVudC9leHRlcm5hbC9hODZmZTQ5Ni1lMzE3LTQ4NDItODYyZi0yZTg3ZjE3NzMwMDIifSx7ImJ1Y2tldCI6ImNsaWVudC1iZXN0aW5zdXJhbmNlLXRydWZsYS1zdGEifSx7IlgtQW16LUFsZ29yaXRobSI6IkFXUzQtSE1BQy1TSEEyNTYifSx7IlgtQW16LUNyZWRlbnRpYWwiOiJBU0lBMjQ2QVFENFRWQVEyNVBIWi8yMDIzMDIwNy9jYS1jZW50cmFsLTEvczMvYXdzNF9yZXF1ZXN0In0seyJYLUFtei1EYXRlIjoiMjAyMzAyMDdUMjExMTU2WiJ9LHsiWC1BbXotU2VjdXJpdHktVG9rZW4iOiJGd29HWlhJdllYZHpFTy8vLy8vLy8vLy8vd0VhREpDbEY5ZkZ4NElRU0xyS3FpTGtBZGYvTDFaQ3U0NVJsNXA3a0FGVnUwWS9OMkdsRytRTzF3L1RGSUhKYU5tVzRkUUl2MFQvSDBMRGNTYWUzMmhXQjltV080N09rQXdSVTN0a09EZWRQOTJtaFl5VWRya1d3cU9Qamk5OWhlRFBPem1sUzlkd2U1ZVBscEtiNXg5alN0VmVmSDdreUgyRW5SZGdtQTBBWnQrQ2d3RzJ4c0RObUJrZDZiY01UazhvZnRmVTllaUo0WWZpbWtZTGV1TVBmdFNNeS9LdEQwNmNTSjhSTk1BdUJBaUg4ck4raEhsckFTamZYT0plb0lMdjM3ellqTmNlZUV3d0hZZStmYjN3czFHbHFObWdvaGd1cDFmOG5FVnR6M3BjVEFDbnNLNmluWjVxanE1bk4xMTViaDkyMXlpYy9ZcWZCakl0UXlpVUdYZXN3Sld1UlJ3SFpSb3ExWjY0NjZSYU1leDh5UmtnNzFBZVhrZWl5eXJwNkJ1QVNEOE1xejNuIn1dfQ==",
-          "X-Amz-Signature": "e264cf5618335e0f52e20f5cbee3a4fd42181f6e08cee0a9fd4c515cc4b3e93f"
+          "X-Amz-Credential": "ASIA246AQD4TX4L2ZLOT/20230219/ca-central-1/s3/aws4_request",
+          "X-Amz-Date": "20230219T104816Z",
+          "X-Amz-Security-Token": "FwoGZXIvYXdzEAQaDGlMtxNtc1kidLeN+iLkAbcubFUtutWZdVKsCCKkv4ceZ2NZ1uhgxBwD+84oexDAliwux47ClIcmL2FGamzU+rwxctgf5RNjYupSqEOuxLnSJq8LijAnb0Xti7zLV4NxjF+UvSDip6wrkv67ftUYCxvCtz3JG28spyar1EryWzbUOVOxzyxJ6PSpeNXkm7M1fBLuQlmMUSjqsMaZHhTsFN629JHRPwGdVx6Oy2+zLH3tV4Ufg+lJIH3oD9QPJ7MbErwzcWkUGL1z0DfqGpYrBPba8IoLYuA8xePYSK/T+6uZfu5siWWeQMCkK5jp8XRjzOTaVSjv/MefBjItPQArG8xNDm9kR1BGFyF6k0adfZ5QpDNYLmMSbOvlMIWmyuNa/vVerAkQEurf",
+          "Policy": "eyJleHBpcmF0aW9uIjoiMjAyMy0wMi0xOVQxMTo0ODoxNloiLCJjb25kaXRpb25zIjpbWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsMSwxMDQ4NTc2MDBdLFsic3RhcnRzLXdpdGgiLCIkQ29udGVudC1UeXBlIiwiIl0seyJLZXkiOiJ0cnVkb2NzL3RyYW5zaWVudC9leHRlcm5hbC8xYWQxYjM3OS1hYjU4LTRkMWYtOGYwZi1mMjY2ZTQ3ZjU2OGIifSx7ImJ1Y2tldCI6ImNsaWVudC1iZXN0aW5zdXJhbmNlLXRydWZsYS1zdGEifSx7IlgtQW16LUFsZ29yaXRobSI6IkFXUzQtSE1BQy1TSEEyNTYifSx7IlgtQW16LUNyZWRlbnRpYWwiOiJBU0lBMjQ2QVFENFRYNEwyWkxPVC8yMDIzMDIxOS9jYS1jZW50cmFsLTEvczMvYXdzNF9yZXF1ZXN0In0seyJYLUFtei1EYXRlIjoiMjAyMzAyMTlUMTA0ODE2WiJ9LHsiWC1BbXotU2VjdXJpdHktVG9rZW4iOiJGd29HWlhJdllYZHpFQVFhREdsTXR4TnRjMWtpZExlTitpTGtBYmN1YkZVdHV0V1pkVktzQ0NLa3Y0Y2VaMk5aMXVoZ3hCd0QrODRvZXhEQWxpd3V4NDdDbEljbUwyRkdhbXpVK3J3eGN0Z2Y1Uk5qWXVwU3FFT3V4TG5TSnE4TGlqQW5iMFh0aTd6TFY0TnhqRitVdlNEaXA2d3JrdjY3ZnRVWUN4dkN0ejNKRzI4c3B5YXIxRXJ5V3piVU9WT3h6eXhKNlBTcGVOWGttN00xZkJMdVFsbU1VU2pxc01hWkhoVHNGTjYyOUpIUlB3R2RWeDZPeTIrekxIM3RWNFVmZytsSklIM29EOVFQSjdNYkVyd3pjV2tVR0wxejBEZnFHcFlyQlBiYThJb0xZdUE4eGVQWVNLL1QrNnVaZnU1c2lXV2VRTUNrSzVqcDhYUmp6T1RhVlNqdi9NZWZCakl0UFFBckc4eE5EbTlrUjFCR0Z5RjZrMGFkZlo1UXBETllMbU1TYk92bE1JV215dU5hL3ZWZXJBa1FFdXJmIn1dfQ==",
+          "X-Amz-Signature": "27af72d0525120fa46f62e726560bced118ced6f16e87e3e5be572241dc09412"
       }
   }
 }
@@ -27,7 +27,8 @@ Object
   }))
 //3d4dd041-72f6-40bd-bdc6-91745577f55e
 form.append('Content-Type', 'application/pdf');
-form.append('file', fs.readFileSync('../PDFs/Quote Template - US.pdf'));
+form.append('file', fs.readFileSync('../PDFs/Quote Template - UK.pdf'));
+// form.append('file', fs.readFileSync('./mapQuotePdfInputs'));
 
 fetch(contactsPost.uploadForm.url, { method: 'POST', body: form })
   .then(res => res.text())
---
+++ Request/united-kingdom/TODO.md
@@ -0,0 +1,5 @@
+Change Status Ket in submit/approve Button to be Quoting Status.
+Change Status Key in admin Decline Button to be NTU Status.
+Change Application UiSchema Key From Quote Screen.
+
+Add if/then/else TO PDF Generations.
