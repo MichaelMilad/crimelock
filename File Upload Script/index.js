e81db96bc77679862f83cb07055a1304da590879
Michael Milad <michaelmilad001@gmail.com>
2023-03-09 13:45:27 +0200

    updates

--- File Upload Script/index.js
+++ File Upload Script/index.js
@@ -3,18 +3,18 @@ const fs = require('fs');
 const formData = require('form-data');
 
 const contactsPost = {
-  "uploadRef": "1ad1b379-ab58-4d1f-8f0f-f266e47f568b",
+  "uploadRef": "993f31b4-f1f5-440c-867b-bd856dee16c2",
   "uploadForm": {
       "url": "https://s3.ca-central-1.amazonaws.com/client-bestinsurance-trufla-sta",
       "fields": {
-          "Key": "trudocs/transient/external/1ad1b379-ab58-4d1f-8f0f-f266e47f568b",
+          "Key": "trudocs/transient/external/993f31b4-f1f5-440c-867b-bd856dee16c2",
           "bucket": "client-bestinsurance-trufla-sta",
           "X-Amz-Algorithm": "AWS4-HMAC-SHA256",
-          "X-Amz-Credential": "ASIA246AQD4TX4L2ZLOT/20230219/ca-central-1/s3/aws4_request",
-          "X-Amz-Date": "20230219T104816Z",
-          "X-Amz-Security-Token": "FwoGZXIvYXdzEAQaDGlMtxNtc1kidLeN+iLkAbcubFUtutWZdVKsCCKkv4ceZ2NZ1uhgxBwD+84oexDAliwux47ClIcmL2FGamzU+rwxctgf5RNjYupSqEOuxLnSJq8LijAnb0Xti7zLV4NxjF+UvSDip6wrkv67ftUYCxvCtz3JG28spyar1EryWzbUOVOxzyxJ6PSpeNXkm7M1fBLuQlmMUSjqsMaZHhTsFN629JHRPwGdVx6Oy2+zLH3tV4Ufg+lJIH3oD9QPJ7MbErwzcWkUGL1z0DfqGpYrBPba8IoLYuA8xePYSK/T+6uZfu5siWWeQMCkK5jp8XRjzOTaVSjv/MefBjItPQArG8xNDm9kR1BGFyF6k0adfZ5QpDNYLmMSbOvlMIWmyuNa/vVerAkQEurf",
-          "Policy": "eyJleHBpcmF0aW9uIjoiMjAyMy0wMi0xOVQxMTo0ODoxNloiLCJjb25kaXRpb25zIjpbWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsMSwxMDQ4NTc2MDBdLFsic3RhcnRzLXdpdGgiLCIkQ29udGVudC1UeXBlIiwiIl0seyJLZXkiOiJ0cnVkb2NzL3RyYW5zaWVudC9leHRlcm5hbC8xYWQxYjM3OS1hYjU4LTRkMWYtOGYwZi1mMjY2ZTQ3ZjU2OGIifSx7ImJ1Y2tldCI6ImNsaWVudC1iZXN0aW5zdXJhbmNlLXRydWZsYS1zdGEifSx7IlgtQW16LUFsZ29yaXRobSI6IkFXUzQtSE1BQy1TSEEyNTYifSx7IlgtQW16LUNyZWRlbnRpYWwiOiJBU0lBMjQ2QVFENFRYNEwyWkxPVC8yMDIzMDIxOS9jYS1jZW50cmFsLTEvczMvYXdzNF9yZXF1ZXN0In0seyJYLUFtei1EYXRlIjoiMjAyMzAyMTlUMTA0ODE2WiJ9LHsiWC1BbXotU2VjdXJpdHktVG9rZW4iOiJGd29HWlhJdllYZHpFQVFhREdsTXR4TnRjMWtpZExlTitpTGtBYmN1YkZVdHV0V1pkVktzQ0NLa3Y0Y2VaMk5aMXVoZ3hCd0QrODRvZXhEQWxpd3V4NDdDbEljbUwyRkdhbXpVK3J3eGN0Z2Y1Uk5qWXVwU3FFT3V4TG5TSnE4TGlqQW5iMFh0aTd6TFY0TnhqRitVdlNEaXA2d3JrdjY3ZnRVWUN4dkN0ejNKRzI4c3B5YXIxRXJ5V3piVU9WT3h6eXhKNlBTcGVOWGttN00xZkJMdVFsbU1VU2pxc01hWkhoVHNGTjYyOUpIUlB3R2RWeDZPeTIrekxIM3RWNFVmZytsSklIM29EOVFQSjdNYkVyd3pjV2tVR0wxejBEZnFHcFlyQlBiYThJb0xZdUE4eGVQWVNLL1QrNnVaZnU1c2lXV2VRTUNrSzVqcDhYUmp6T1RhVlNqdi9NZWZCakl0UFFBckc4eE5EbTlrUjFCR0Z5RjZrMGFkZlo1UXBETllMbU1TYk92bE1JV215dU5hL3ZWZXJBa1FFdXJmIn1dfQ==",
-          "X-Amz-Signature": "27af72d0525120fa46f62e726560bced118ced6f16e87e3e5be572241dc09412"
+          "X-Amz-Credential": "ASIA246AQD4TXDJOS5EZ/20230309/ca-central-1/s3/aws4_request",
+          "X-Amz-Date": "20230309T105004Z",
+          "X-Amz-Security-Token": "FwoGZXIvYXdzELT//////////wEaDJ5ZHUMXGQFB5N47QyLkAYzqcUCGQJDFkMiFOGXmqQKfCBLJlIG4NoDAmVjQ6+w7gNJTr+lJ64A9RYLu/PsirD6BFmF/DUGwRWqWhPRPsLImZPDxI123Bw1slXvAZ7/Bo5w0aRqywBpAeajOHAyr39K64efadlaUXM3KkTERtS7BLNA//hbd9EXbtP5bWvruogPelt7JGJNAUAljuQrJB8fnimdangfOE+WzE/AFSjOIclr1FllsylDbJntVqNWNhhaOBssmcgCxh61/4vmpscsJ98wKjhgnxF8vjS9jNwGTUhwgeytGbHDioC0Pqt5jdRBTqCjc86agBjItuZVSY8lFI883roCwPi2WRfPm0YicTqoR9Kn8UAyvEEiT32CcxeQGGNhB1Jg4",
+          "Policy": "eyJleHBpcmF0aW9uIjoiMjAyMy0wMy0wOVQxMTo1MDowNFoiLCJjb25kaXRpb25zIjpbWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsMSwxMDQ4NTc2MDBdLFsic3RhcnRzLXdpdGgiLCIkQ29udGVudC1UeXBlIiwiIl0seyJLZXkiOiJ0cnVkb2NzL3RyYW5zaWVudC9leHRlcm5hbC85OTNmMzFiNC1mMWY1LTQ0MGMtODY3Yi1iZDg1NmRlZTE2YzIifSx7ImJ1Y2tldCI6ImNsaWVudC1iZXN0aW5zdXJhbmNlLXRydWZsYS1zdGEifSx7IlgtQW16LUFsZ29yaXRobSI6IkFXUzQtSE1BQy1TSEEyNTYifSx7IlgtQW16LUNyZWRlbnRpYWwiOiJBU0lBMjQ2QVFENFRYREpPUzVFWi8yMDIzMDMwOS9jYS1jZW50cmFsLTEvczMvYXdzNF9yZXF1ZXN0In0seyJYLUFtei1EYXRlIjoiMjAyMzAzMDlUMTA1MDA0WiJ9LHsiWC1BbXotU2VjdXJpdHktVG9rZW4iOiJGd29HWlhJdllYZHpFTFQvLy8vLy8vLy8vd0VhREo1WkhVTVhHUUZCNU40N1F5TGtBWXpxY1VDR1FKREZrTWlGT0dYbXFRS2ZDQkxKbElHNE5vREFtVmpRNit3N2dOSlRyK2xKNjRBOVJZTHUvUHNpckQ2QkZtRi9EVUd3UldxV2hQUlBzTEltWlBEeEkxMjNCdzFzbFh2QVo3L0JvNXcwYVJxeXdCcEFlYWpPSEF5cjM5SzY0ZWZhZGxhVVhNM0trVEVSdFM3QkxOQS8vaGJkOUVYYnRQNWJXdnJ1b2dQZWx0N0pHSk5BVUFsanVRckpCOGZuaW1kYW5nZk9FK1d6RS9BRlNqT0ljbHIxRmxsc3lsRGJKbnRWcU5XTmhoYU9Cc3NtY2dDeGg2MS80dm1wc2NzSjk4d0tqaGdueEY4dmpTOWpOd0dUVWh3Z2V5dEdiSERpb0MwUHF0NWpkUkJUcUNqYzg2YWdCakl0dVpWU1k4bEZJODgzcm9Dd1BpMldSZlBtMFlpY1Rxb1I5S244VUF5dkVFaVQzMkNjeGVRR0dOaEIxSmc0In1dfQ==",
+          "X-Amz-Signature": "b213176c6385e533ad118ae43ee48eece81456680a6b5b224156a97b4b08eb7e"
       }
   }
 }
@@ -27,8 +27,8 @@ Object
   }))
 //3d4dd041-72f6-40bd-bdc6-91745577f55e
 form.append('Content-Type', 'application/pdf');
-form.append('file', fs.readFileSync('../PDFs/Quote Template - UK.pdf'));
-// form.append('file', fs.readFileSync('./mapQuotePdfInputs'));
+form.append('file', fs.readFileSync('../PDFs/Cert Template - UK.pdf'));
+// form.append('file', fs.readFileSync('./mapFinalBoundQuotes'));
 
 fetch(contactsPost.uploadForm.url, { method: 'POST', body: form })
   .then(res => res.text())
